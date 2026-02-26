6ecb4d08278b0fb8a6715406c8efabc6c3f8c286
yyu3333 <yyu3333@users.noreply.github.com>
2026-02-27 00:06:52 +0100

    Fix duplicate extension entries

--- .github/scripts/move-apks.py
+++ .github/scripts/move-apks.py
@@ -3,8 +3,12 @@ import shutil
 
 # Move to apk folder
 REPO_DIR = Path(".")
-(REPO_DIR / "apk").mkdir(parents=True, exist_ok=True)
+apk_dir = REPO_DIR / "apk"
+
+if apk_dir.exists():
+    shutil.rmtree(apk_dir)
+apk_dir.mkdir(parents=True, exist_ok=True)
 
 for apk in (Path.home() / "apk-artifacts").glob("**/*.apk"):
-    apk_name = apk.name.replace("-release.apk", ".apk").replace("-debug.apk", ".apk")
-    shutil.move(apk, REPO_DIR / "apk" / apk_name)
+    apk_name = apk.name.replace("-release-unsigned.apk", ".apk").replace("-release.apk", ".apk").replace("-debug.apk", ".apk")
+    shutil.move(apk, apk_dir / apk_name)
